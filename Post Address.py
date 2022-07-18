12b789043bab883912af0b625a5a666d1c3ed9f9
MANYAM RAMESH REDDY <90704596+MANYAMRAMESHREDDY@users.noreply.github.com>
2022-07-18 14:29:25 +0530

    Add files via upload

---
+++ Post Address.py
@@ -0,0 +1,17 @@
+#HELLO EVERY ONE THIS MANYAM RAMESH REDDY
+
+
+Country=input("Country Name:")
+State=input("State Name:")
+District=input("District Name:")
+Mandal=input("Mandal Name:")
+Panchayathi=input("Panchayathi Name:")
+Village=input("Village Name:")
+Pincode=int(input("Pincode Number:"))
+print("\nAddress Details:",Country,State,District,Mandal,Panchayathi,Village,Pincode,sep="\n")
+
+
+
+
+
+
